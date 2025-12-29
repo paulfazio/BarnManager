d09fa10f62e5748bfd8a7a67395fa39b791f428d
Paul Fazio <paul.fazio@gmail.com>
2025-12-28 19:41:31 -0500

    Weather is not updating correctly

--- app.py
+++ app.py
@@ -218,4 +218,4 @@ def delete_horse(horse_id):
     return redirect(url_for("configure_horses"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
